3b9e654b1f6d16197bac22e7a2d413f137669e6b
Isamu <support@isamu.dev>
2022-09-05 19:03:52 -0400

    feat: challenge information and html structure

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    Improve your front-end skills by building projects Scan the QR code to visit
+    Frontend Mentor and take your coding skills to the next level
+
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://isamu.dev">@isamudev</a>.
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -7,12 +18,13 @@ useHead({
   htmlAttrs: {
     lang: 'en',
   },
-  title: '',
+  title: 'QR code component challenge on Frontend Mentor',
   titleTemplate: (title) => `${title} - @isamudev | Full Stack Developer`,
   meta: [
     {
       name: 'description',
-      content: '',
+      content:
+        'This is a solution to the QR code component challenge on Frontend Mentor.',
     },
   ],
   link: [
