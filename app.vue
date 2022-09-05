d787f5f7f4e90943ae99f8f6472d7755dabf7a95
Isamu <support@isamu.dev>
2022-09-05 18:56:02 -0400

    feat(struct): skeleton template

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  title: '',
+  titleTemplate: (title) => `${title} - @isamudev | Full Stack Developer`,
+  meta: [
+    {
+      name: 'description',
+      content: '',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.ico',
+      sizes: '32x32',
+    },
+  ],
+})
+</script>
